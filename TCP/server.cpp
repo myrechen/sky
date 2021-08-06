707859959ad39d5a85cb944800626d9ebaef558b
myrechen <1972375779@qq.com>
2021-08-06 21:01:44 +0800

    linux_server

--- TCP/server.cpp
+++ TCP/server.cpp
@@ -1,78 +1,82 @@
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <stdio.h>
+#include "stdlib.h"
+#include "assert.h"
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <sys/un.h>
 #include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <netdb.h>
-#include <errno.h>
-#define PORT 2345
-#define MAXSIZE 1024
-int main(int argc, char *argv[])
+int main()
 {
-int sockfd, newsockfd;
-//定义服务端套接口数据结构
-struct sockaddr_in server_addr;
-struct sockaddr_in client_addr;
-int sin_zise, portnumber;
-//发送数据缓冲区
-char buf[MAXSIZE];
-//定义客户端套接口数据结构
-int addr_len = sizeof(struct sockaddr_in);
-if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-{
-fprintf(stderr, "create socket failed\n");
-exit(EXIT_FAILURE);
-}
-puts("create socket success");
-printf("sockfd is %d\n", sockfd);
-//清空表示地址的结构体变量
-bzero(&server_addr, sizeof(struct sockaddr_in));
-//设置addr的成员变量信息
-server_addr.sin_family = AF_INET;
-server_addr.sin_port = htons(PORT);
-//设置ip为本机IP
-server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-if (bind(sockfd, (struct sockaddr*)(&server_addr), sizeof(struct sockaddr)) < 0)
-{
-fprintf(stderr, "bind failed \n");
-exit(EXIT_FAILURE);
-}
-puts("bind success\n");
-if (listen(sockfd, 10) < 0)
-{
-perror("listen fail\n");
-exit(EXIT_FAILURE);
-}
-puts("listen success\n");
-int sin_size = sizeof(struct sockaddr_in);
-printf("sin_size is %d\n", sin_size);
-if ((newsockfd = accept(sockfd, (struct sockaddr *)(&client_addr), &sin_size)) < 0)
-{
-perror("accept error");
-exit(EXIT_FAILURE);
-}
-printf("accepted a new connetction\n");
-printf("new socket id is %d\n", newsockfd);
-printf("Accept clent ip is %s\n", inet_ntoa(client_addr.sin_addr));
-printf("Connect successful please input message\n");
-char sendbuf[1024];
-char mybuf[1024];
-while (1)
-{
-int len = recv(newsockfd, buf, sizeof(buf), 0);
-if (strcmp(buf, "exit\n") == 0)
-break;
-fputs(buf, stdout);
-send(newsockfd, buf, len, 0);
-memset(sendbuf, 0 ,sizeof(sendbuf));
-memset(buf, 0, sizeof(buf));
-}
-close(newsockfd);
-close(sockfd);
-puts("exit success");
-exit(EXIT_SUCCESS);
-return 0;
+	//定义发送缓冲区和接受缓冲区
+    char send_buf[100];
+    char recv_buf[100];
+	int send_len = 0;
+    int recv_len = 0;
+
+	int bind_status,listen_status;
+
+	//定义服务端套接字，接受请求套接字
+	int server_sockfd, client_sockfd;
+	server_sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+	socklen_t server_len, client_len;
+
+	//服务端地址客户端地址
+	struct sockaddr_in server_address;
+	struct sockaddr_in client_address;
+	//填充服务端信息
+	memset(&server_address, 0, sizeof(server_address));
+	server_address.sin_family = AF_INET;
+	server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+	server_address.sin_port = htons(1234);	
+
+	int opt = 1,ret;
+	ret = setsockopt(server_sockfd,SOL_SOCKET,SO_REUSEADDR,&opt,sizeof( opt ));	
+	if(ret == -1)
+		printf("set failed\n");
+	if(ret == 0)
+		printf("seccess\n");
+
+	server_len = sizeof(server_address);
+	bind_status = bind(server_sockfd, (struct sockaddr*)&server_address, server_len);
+	if(bind_status == -1)
+		printf("socket bind fail\n");
+	else
+		printf("socket bind success\n");
+	
+	listen_status = listen(server_sockfd, 5);
+	if(listen_status == 0)
+		printf("listen success\n");
+	else
+		printf("listen fail\n");
+	
+	printf("server listening\n");
+
+	client_len = sizeof(client_address);
+	client_sockfd = accept(server_sockfd, (struct sockaddr*)&client_address, &client_len);
+	if(client_sockfd == -1)
+	{	printf("connect fail");
+		return 0;
+	}
+	printf("recv data\n");
+
+	//接收数据
+	while(1) 
+	{
+		memset(recv_buf,0,sizeof(recv_buf));
+		recv_len = recv(client_sockfd, recv_buf, 100, 0);
+        if (recv_len < 0) {
+            printf("receive fail\n");
+            break;
+        }
+		else
+			printf("client msg:%s\n",recv_buf);
+		printf("please send msg:");
+		scanf(" %s",send_buf);
+		send_len = send(client_sockfd, send_buf, sizeof(send_buf), 0);
+	}
+	close(client_sockfd);
+	close(server_sockfd);
 }
